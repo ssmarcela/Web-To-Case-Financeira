049e74d354eb34043c7d87a834689c31ada956f6
Marcela Santos <ssmarcela@outlook.com>
2025-01-23 17:23:10 -0300

    Primeiro commit: projeto Web-to-Case com React e Material UI

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Box, TextField, Button } from "@mui/material";
+import { Typography } from "@mui/material";
+
+function App() {
+    return (
+        <Box
+            component="form"
+            action="https://webto.salesforce.com/servlet/servlet.WebToCase?encoding=UTF-8&orgId=00Daj00000L1nZv"
+            method="POST"
+            sx={{
+                maxWidth: 500,
+                margin: "20px auto",
+                padding: 3,
+                border: "1px solid #ccc",
+                borderRadius: 2,
+                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+            }}
+        >
+
+         <Typography variant="h4" component="h2" color="success">Reportar Problemas com a sua Transação</Typography>
+
+         <Typography variant="subtitle1" gutterBottom>
+                Por favor, preencha os dados abaixo para reportar sua transação.
+         </Typography>
+
+            <input type="hidden" name="orgid" value="00Daj00000L1nZv" />
+            <input type="hidden" name="retURL" value="http://google.com" />
+
+            <TextField
+                label="Nome do Contato"
+                name="name"
+                fullWidth
+                margin="normal"
+                required
+            />
+            <TextField
+                label="Email"
+                name="email"
+                type="email"
+                fullWidth
+                margin="normal"
+                required
+            />
+            <TextField
+                label="Telefone"
+                name="phone"
+                type="tel"
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Descrição"
+                name="description"
+                fullWidth
+                multiline
+                rows={4}
+                margin="normal"
+            />
+            <Button type="submit" variant="contained" color="success" fullWidth>
+                Enviar
+            </Button>
+        </Box>
+    );
+}
+
+export default App;
