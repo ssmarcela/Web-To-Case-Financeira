3b7f95e16f47fc4d3af64b7e3bf5f845da1f0838
Marcela Santos <ssmarcela@outlook.com>
2025-01-24 17:03:01 -0300

    Adicionando novos campos no formulário

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Box, TextField, Button } from "@mui/material";
-import { Typography } from "@mui/material";
+import { Box, TextField, Button, MenuItem, Typography } from "@mui/material";
 
 function App() {
     return (
@@ -17,12 +16,13 @@ function App() {
                 boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
             }}
         >
+            <Typography variant="h4" component="h2" color="success" gutterBottom>
+                Reportar Problemas com a sua Transação
+            </Typography>
 
-         <Typography variant="h4" component="h2" color="success">Reportar Problemas com a sua Transação</Typography>
-
-         <Typography variant="subtitle1" gutterBottom>
+            <Typography variant="subtitle1" gutterBottom>
                 Por favor, preencha os dados abaixo para reportar sua transação.
-         </Typography>
+            </Typography>
 
             <input type="hidden" name="orgid" value="00Daj00000L1nZv" />
             <input type="hidden" name="retURL" value="http://google.com" />
@@ -57,7 +57,36 @@ function App() {
                 rows={4}
                 margin="normal"
             />
-            <Button type="submit" variant="contained" color="success" fullWidth>
+            <TextField
+                label="Código da Transação"
+                name="00Naj00000I8uvV"
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Valor da Transação"
+                name="00Naj00000I8vA1"
+                type="number"
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                select
+                label="Tipo de Reclamação"
+                name="00Naj00000I8vMw"
+                fullWidth
+                margin="normal"
+            >
+                <MenuItem value="">-- Nenhum --</MenuItem>
+                <MenuItem value="Compra no cartão não reconhecida">
+                    Compra no cartão não reconhecida
+                </MenuItem>
+                <MenuItem value="Outros">Outros</MenuItem>
+                <MenuItem value="Pix enviado por engano">Pix enviado por engano</MenuItem>
+                <MenuItem value="Transação desconhecida">Transação desconhecida</MenuItem>
+            </TextField>
+
+            <Button type="submit" variant="contained" color="success" fullWidth sx={{ marginTop: 2 }}>
                 Enviar
             </Button>
         </Box>
